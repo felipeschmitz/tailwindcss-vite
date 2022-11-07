220c085f648ab08ddfa24c89062667167bfbec14
Felipe Schmitz <schmitz.felipe@gmail.com>
2022-11-07 08:33:31 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-indigo-500 flex flex-col justify-center items-center">
+      <h1 className="text-white text-5xl text-center mb-4">
+        ViteJS + ReactJS + eslint + prettier + tailwindcss + react-icons +
+        headlessui
+      </h1>
+      <p className="text-white text-6xl font-semibold">Starter Kit</p>
+    </div>
+  )
+}
+
+export default App
